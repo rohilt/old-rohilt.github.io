0653b5e64dd0e82667552bc7b772a1ab7cc07ea7
Rohil Tuli <rohil.tuli@gmail.com>
2019-12-16 14:21:20 -0500

    Updates

---
+++ precache-manifest.f48864ce14f136f1c71b75fbd68201e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3111f5c589cc920bd905695bc5236a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d34174741775af379316",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c8087d575e0b08e8aa37",
+    "url": "/static/js/2.b68b048e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b68b048e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d34174741775af379316",
+    "url": "/static/js/main.6a32e305.chunk.js"
+  },
+  {
+    "revision": "dbb06af8e9f5c6c3b28d",
+    "url": "/static/js/runtime-main.eceace76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
